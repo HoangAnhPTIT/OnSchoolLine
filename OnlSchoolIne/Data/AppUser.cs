5d743e6508c963c0c12eeec2760fc3ae7becdbe5
AnhCH <anhch@tuivietnam.com>
2022-06-25 13:19:37 +0700

    base project

---
+++ OnlSchoolIne/Data/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlSchoolIne.Data
+{
+    [Table("IdentityUser")]
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ OnlSchoolIne/Infrastructure/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlSchoolIne.Data;
+
+namespace OnlSchoolIne.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+    }
+}
